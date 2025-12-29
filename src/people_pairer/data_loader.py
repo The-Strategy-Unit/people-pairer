4834bf2626d443b33f339b8091b437230ed5e997
YiWen Hon <yiwen.hon1@nhs.net>
2025-12-29 15:40:40 +0000

    add functionality to load all participants

--- src/people_pairer/data_loader.py
+++ src/people_pairer/data_loader.py
@@ -1,12 +1,27 @@
+from people_pairer.utils import setup_logger
 import pandas as pd
+import os
 
-
-def csv_validator(filename: str) -> bool:
-    if filename.endswith(".csv"):
-        return True
-    return False
+logger = setup_logger(__name__, level="INFO")
 
 
 def load_csv(filename: str) -> pd.DataFrame:
-    with open(filename) as f:
-        return pd.read_csv(f)
+    """Loads CSV file
+
+    Args:
+        filename (str): CSV file to load
+
+    Returns:
+        pd.DataFrame: Contents of loaded CSV file
+    """
+    try:
+        with open(filename) as f:
+            return pd.read_csv(f, encoding="utf-8")
+    except Exception:
+        logger.exception("Failed to load CSV: %s", filename)
+        raise
+
+
+def load_participants() -> pd.DataFrame:
+    participants = load_csv(os.path.join("data", "participants.csv"))
+    return participants[participants["active"]]
--- src/people_pairer/main.py
+++ src/people_pairer/main.py
@@ -1,18 +1,17 @@
 from people_pairer.utils import setup_logger
-from people_pairer.data_loader import csv_validator
+from people_pairer.data_loader import load_participants
 
 logger = setup_logger(__name__, level="INFO")
 
 
 def main():
-    logger.info("Starting application")
-    logger.info(csv_validator("data/active_participants.csv"))
-    logger.info("Application finished")
+    logger.info("🍐 Loading pairing participants...")
+
     ### PSEUDOCODE BELOW
-    # Load users for current iteration (CSV)
     # Load past pairs (CSV)
     # Run matching
     # Output another CSV
+    logger.info("Application finished")
 
 
 if __name__ == "__main__":
