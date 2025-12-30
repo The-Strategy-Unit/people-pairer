7437564b3667118af4fa74e35180075c5b8e8ab9
YiWen Hon <yiwen.hon1@nhs.net>
2025-12-30 10:19:45 +0000

    add functionality to load past pairs

--- src/people_pairer/data_loader.py
+++ src/people_pairer/data_loader.py
@@ -2,6 +2,7 @@ from people_pairer.utils import setup_logger
 import pandas as pd
 import os
 
+
 logger = setup_logger(__name__, level="INFO")
 
 
@@ -25,3 +26,28 @@ def load_csv(filename: str) -> pd.DataFrame:
 def load_participants() -> pd.DataFrame:
     participants = load_csv(os.path.join("data", "participants.csv"))
     return participants[participants["active"]]
+
+
+def list_past_pairings(folder_name: str):
+    try:
+        past_pairings = sorted(
+            [
+                os.path.join(folder_name, "pairings", file)
+                for file in os.listdir(os.path.join(folder_name, "pairings"))
+                if file.startswith("round_")
+            ]
+        )
+        logger.info(f"{len(past_pairings)} past pairings found")
+        return past_pairings
+    except Exception:
+        logger.exception(f"Failed to find folder: {folder_name}")
+        raise
+
+
+def load_past_pairings(folder_name: str = "data"):
+    past_pairings = list_past_pairings(folder_name)
+    recent_pairings = past_pairings[-3:]
+    recent_pairing_dfs = []
+    if len(recent_pairings) > 0:
+        recent_pairing_dfs = [pd.read_csv(round_file) for round_file in recent_pairings]
+    return recent_pairing_dfs
--- src/people_pairer/main.py
+++ src/people_pairer/main.py
@@ -1,14 +1,16 @@
 from people_pairer.utils import setup_logger
-from people_pairer.data_loader import load_participants
+from people_pairer.data_loader import load_participants, load_past_pairings
 
 logger = setup_logger(__name__, level="INFO")
 
 
 def main():
     logger.info("🍐 Loading pairing participants...")
-
+    participants = load_participants()
     ### PSEUDOCODE BELOW
     # Load past pairs (CSV)
+    logger.info("Loading past pairs...")
+    past_pairing_dfs = load_past_pairings()
     # Run matching
     # Output another CSV
     logger.info("Application finished")
